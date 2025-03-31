10d74ecb14a0d6820c1b43d61bef12f1850cbfac
Goga <g.bagauri@yahoo.com>
2025-04-01 00:17:23 +0400

    main frame is done. need more styling

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="min-h-screen  flex items-center justify-center p-6">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
